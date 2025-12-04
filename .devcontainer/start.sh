b9949a4897251fdd230b6caefd6afee0c2058727
Dries Swinnen <36841875+d-ries@users.noreply.github.com>
2025-12-04 14:45:32 +0100

    Add start.sh script for setup and auto-commit

---
+++ .devcontainer/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# setup log links
+LOG_SCRIPT_PATH=$(find /workspaces -name "setup-log-links.sh" -type f 2>/dev/null | head -n 1)
+bash "$LOG_SCRIPT_PATH"
+
+COMMIT_SCRIPT_PATH=$(find /workspaces -name "auto-commit.sh" -type f 2>/dev/null | head -n 1)
+
+if [ -f "$COMMIT_SCRIPT_PATH" ]; then
+    cd "$(dirname "$COMMIT_SCRIPT_PATH")/.." || exit
+    setsid bash "$COMMIT_SCRIPT_PATH" > /tmp/auto-commit.log 2>&1 < /dev/null &
+    SCRIPT_PID=$!
+    echo "Script started with PID: $SCRIPT_PID" >> /tmp/wrapper.log
+
+    sleep 2
+fi
